3f89b8ce90837dbb01366f3a2283fd2255ccb552
qvortex-beem <ghujfeuhh1@gmail.com>
2026-02-10 23:57:43 +0300

    step9: form validation in js

--- main.js
+++ main.js
@@ -47,5 +47,20 @@ form.addEventListener("submit", (event) => {
    event.preventDefault();
    const name = usernameInput.value;
    const age = ageInput.value;
-   result.textContent = `Имя: ${name}, Возраст: ${age}`;
+   if (name === "") {
+      result.textContent = "Ошибка: Имя не может быть пустым!";
+      result.style.color = "red";
+      usernameInput.focus();
+      return;
+   }
+   if (isNaN(age) || age <= 0 || age > 120) {
+      result.textContent = "Ошибка: Введите корректный возраст (от 1 до 120)!";
+      result.style.color = "red";
+      usernameInput.focus();
+      ageInput.value = "";
+      return;
+   }
+   result.textContent = `Данные сохранены: ${name}, ${age} лет`;
+   result.style.color = "green";
+   form.reset();
 });
